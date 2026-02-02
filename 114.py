c0fa3ab22feb1fafe5f7487ef79aa59cc040b188
bolatovarman <armanieboy08@gmail.com>
2026-02-02 11:19:49 +0500

    Add files via upload

---
+++ 114.py
@@ -0,0 +1,3 @@
+a=input().strip()
+b=int(input())
+print("Hello, "+a+"."+" You are "+b+" years old.")
---
+++ 115.py
@@ -0,0 +1,4 @@
+long_str = input().strip()
+short_str = input().strip()
+
+print(short_str in long_str)
